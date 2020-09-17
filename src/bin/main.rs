8e1d37e018de5b52382e336ee85798fcc98c1457
Frederik Semmel <frederiksemmel@gmail.com>
2020-09-17 16:02:44 +0200

    smoother lines, 8x points per grid segment

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -18,12 +18,6 @@ struct Model {
     x_max: f32,
     y_min: f32,
     y_max: f32,
-    left_bound_active: bool,
-    right_bound_active: bool,
-    top_bound_active: bool,
-    bottom_bound_active: bool,
-    zoom_active: bool,
-    resolution_active: bool,
 }
 
 widget_ids! {
@@ -40,12 +34,7 @@ widget_ids! {
 
 fn model(app: &App) -> Model {
     // Set the loop mode to wait for events, an energy-efficient option for pure-GUI apps.
-    app.new_window()
-        .mouse_wheel(mouse_wheel)
-        .key_pressed(key_pressed)
-        .key_released(key_released)
-        .build()
-        .unwrap();
+    app.new_window().mouse_wheel(mouse_wheel).build().unwrap();
     app.set_loop_mode(LoopMode::Wait);
 
     // Create the UI.
@@ -55,22 +44,14 @@ fn model(app: &App) -> Model {
     let ids = Ids::new(ui.widget_id_generator());
 
     // Init our variables
-    let scale = 100.0;
+    let scale = 5.0;
     let position = pt2(0.0, 0.0);
     let apply_function = true;
     let resolution = 2.0;
-    let x_min = -5.0;
-    let x_max = 5.0;
-    let y_min = -5.0;
-    let y_max = 5.0;
-    let (
-        left_bound_active,
-        right_bound_active,
-        top_bound_active,
-        bottom_bound_active,
-        zoom_active,
-        resolution_active,
-    ) = (false, false, false, false, false, false);
+    let x_min = -0.0;
+    let x_max = 1.0;
+    let y_min = 0.0;
+    let y_max = 1.0;
 
     Model {
         ui,
@@ -83,69 +64,32 @@ fn model(app: &App) -> Model {
         x_max,
         y_min,
         y_max,
-        right_bound_active,
-        left_bound_active,
-        top_bound_active,
-        bottom_bound_active,
-        zoom_active,
-        resolution_active
     }
 }
 
-fn key_pressed(_app: &App, model: &mut Model, key: Key) {
-    match key {
-        Key::A => model.left_bound_active = true,
-        Key::D => model.right_bound_active = true,
-        Key::W => model.top_bound_active = true,
-        Key::S => model.bottom_bound_active = true,
-        Key::LShift => model.zoom_active = true,
-        Key::Space => model.apply_function = !model.apply_function,
-        Key::LControl => model.resolution_active = true,
-        _ => {}
-    }
-}
-
-fn key_released(_app: &App, model: &mut Model, key: Key) {
-    match key {
-        Key::A => model.left_bound_active = false,
-        Key::D => model.right_bound_active = false,
-        Key::W => model.top_bound_active = false,
-        Key::S => model.bottom_bound_active = false,
-        Key::LShift => model.zoom_active = false,
-        Key::LControl => model.resolution_active = false,
-        _ => {}
-    }
-}
-
-fn mouse_wheel(_app: &App, model: &mut Model, dt: MouseScrollDelta, _phase: TouchPhase) {
+fn mouse_wheel(app: &App, model: &mut Model, dt: MouseScrollDelta, _phase: TouchPhase) {
     match dt {
         MouseScrollDelta::PixelDelta(pos) => {
-            if model.left_bound_active {
+            if app.keys.down.contains(&Key::A) {
                 model.x_min -= pos.x as f32 / pow(2.0, model.resolution as usize) as f32 / 2.0
             }
-            if model.right_bound_active {
+            if app.keys.down.contains(&Key::D) {
                 model.x_max -= pos.x as f32 / pow(2.0, model.resolution as usize) as f32 / 2.0
             }
-            if model.top_bound_active {
+            if app.keys.down.contains(&Key::W) {
                 model.y_max -= pos.y as f32 / pow(2.0, model.resolution as usize) as f32 / 2.0
             }
-            if model.bottom_bound_active {
+            if app.keys.down.contains(&Key::S) {
                 model.y_min -= pos.y as f32 / pow(2.0, model.resolution as usize) as f32 / 2.0
             }
-            if model.zoom_active {
-                model.scale += pos.y as f32
+            if app.keys.down.contains(&Key::LShift) {
+                model.scale -= pos.y as f32 / 10.0
             }
-            if model.resolution_active {
-                model.resolution += pos.y as f32 / 20.0
+            if app.keys.down.contains(&Key::LControl) {
+                model.resolution -= pos.y as f32 / 20.0
             }
-            if !model.left_bound_active
-                && !model.right_bound_active
-                && !model.top_bound_active
-                && !model.bottom_bound_active
-                && !model.zoom_active
-                && !model.resolution_active
-            {
-                model.position -= pt2(pos.x as f32 * 8.0, pos.y as f32 * 8.0)
+            if app.keys.down.is_empty() {
+                model.position -= pt2(pos.x as f32, pos.y as f32) / model.scale.exp() * 10.0
             }
         }
         _ => {}
@@ -236,8 +180,8 @@ fn view(app: &App, model: &Model, frame: Frame) {
         model.y_min,
         model.y_max,
     );
-    let points = points.map(|z| if model.apply_function { z * z } else { z });
-    let mut points = points.map(|z| pt2(z.re, z.im) * model.scale + model.position);
+    let points = points.map(|z| if model.apply_function { (z * z).exp() } else { z });
+    let mut points = points.map(|z| (pt2(z.re, z.im) + model.position) * model.scale.exp());
 
     // Begin drawing
     let draw = app.draw();
--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,41 @@ pub fn create_gridlines(
     x_max: f32,
     y_min: f32,
     y_max: f32,
-) -> (impl Iterator<Item = Complex<f32>>, Vec<usize>)  {
-    let resolution = pow(2, resolution.floor().min(5.0) as usize) as f32;
+) -> (impl Iterator<Item = Complex<f32>>, Vec<usize>) {
+    let points_per_grid = 8f32;
+    let resolution = pow(2.0f32, resolution.floor().min(7.0) as usize) * points_per_grid;
     let x_min = (x_min * resolution).ceil() / resolution;
+    let x_coarse_min = (x_min * (resolution / points_per_grid) as f32).ceil()
+        / (resolution / points_per_grid) as f32;
     let x_max = (x_max * resolution).floor() / resolution;
+    let x_coarse_max = (x_max * (resolution / points_per_grid) as f32).floor()
+        / (resolution / points_per_grid) as f32;
     let y_min = (y_min * resolution).ceil() / resolution;
+    let y_coarse_min = (y_min * (resolution / points_per_grid) as f32).ceil()
+        / (resolution / points_per_grid) as f32;
     let y_max = (y_max * resolution).floor() / resolution;
-    let x_len: i16 = ((x_max - x_min) * resolution) as i16 + 1;
-    let y_len: i16 = ((y_max - y_min) * resolution) as i16 + 1;
-    let xs = (0i16..x_len)
-        .map(std::convert::From::from)
-        .map(move |x : f32| x / resolution + x_min);
-    let ys = (0i16..y_len)
-        .map(std::convert::From::from)
-        .map(move |y : f32| y / resolution + y_min);
+    let y_coarse_max = (y_max * (resolution / points_per_grid) as f32).floor()
+        / (resolution / points_per_grid) as f32;
 
-    let x_lines = ys
-        .flat_map(move |y| xs.clone().map(move |x| Complex::new(x, y)));
-    let xs = (0i16..x_len)
-        .map(std::convert::From::from)
-        .map(move |x : f32| x / resolution + x_min);
-    let ys = (0i16..y_len)
-        .map(std::convert::From::from)
-        .map(move |y : f32| y / resolution + y_min);
-    let y_lines = xs
-        .flat_map(move |x| ys.clone().map(move |y| Complex::new(x, y)));
+    let x_len = ((x_max - x_min) * resolution) as usize +1;
+    let x_coarse_len = ((x_coarse_max - x_coarse_min) * (resolution / points_per_grid)) as usize +1;
+    let y_len = ((y_max - y_min) * resolution) as usize +1;
+    let y_coarse_len = ((y_coarse_max - y_coarse_min) * (resolution / points_per_grid)) as usize +1;
+
+    let xs = (0i16..x_len as i16).map(move |x| x as f32 / resolution + x_min);
+    let ys_coarse = (0i16..y_coarse_len as i16)
+        .map(move |x| x as f32 / (resolution / points_per_grid) + y_coarse_min);
+    let x_lines = ys_coarse.flat_map(move |y| xs.clone().map(move |x| Complex::new(x, y)));
+
+    let ys = (0i16..y_len as i16).map(move |y| y as f32 / resolution + y_min);
+    let xs_coarse = (0i16..x_coarse_len as i16)
+        .map(move |x| x as f32 / (resolution / points_per_grid) + x_coarse_min);
+    let y_lines = xs_coarse.flat_map(move |x| ys.clone().map(move |y| Complex::new(x, y)));
+
     let points = x_lines.chain(y_lines);
-    let mut line_structure = vec![x_len as usize; std::cmp::max(0,y_len) as usize];
-    line_structure.extend(vec![y_len as usize; std::cmp::max(0,x_len) as usize]);
+    let mut line_structure = vec![x_len; y_coarse_len];
+    line_structure.extend(vec![y_len; x_coarse_len]);
+
     (points, line_structure)
 }
 pub fn create_gridlines_simple(
